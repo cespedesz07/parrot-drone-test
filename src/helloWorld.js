a88454d277c3eb6da8447c28889b46f49ef3d340
Santiago Céspedes <santiago.cespedes@rms.com>
2017-03-09 22:03:40 -0500

    Script modified with measures to start moving the drone straight and rotating to right 90 deg

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,14 +1,49 @@
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 
-client.takeoff();
+// rightCorrection = function(this){
+// 	this.right(0.06);
+//   	this.left(0.04);
+//   	this.stop();
+// };
+
+client.takeoff(); //3 secs
 
 client
-  .after(5000, function() {
-  	//client.left(0.1);
-  	client.back(0.2);
+  .after(3000, function() {
+  	this.up(0.5);
+  	this.front(0.05);
   })
-  .after(2000, function(){
+  .after(500, function(){
+	this.stop();
+  	this.back(0.04)
   	this.stop();
+  })
+  .after(500, function(){
+  	this.right(0.06);
+  	this.left(0.04);
+  	this.stop();
+  })
+  .after(500, function(){
+  	this.front(0.1);
+  })
+   .after(4000, function(){
+  	this.right(0.07);
+  	this.left(0.04);
+  	this.stop();
+  })
+   .after(500, function(){
+   	this.front(0.1);
+   })
+   .after(2000, function(){
+	this.stop();
+  	this.back(0.8)
+  	this.stop();
+  })
+   .after(1000, function(){
+   	this.counterClockwise(0.5);
+   })
+   .after(2000, function(){
+   	this.stop();
   	this.land();
   });
